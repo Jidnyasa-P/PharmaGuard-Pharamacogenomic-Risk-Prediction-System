261ecf7485c810e1adc967de81f8e56dd4c225a7
Jidnyasa-P <jidnyasapatil019@gmail.com>
2026-03-08 12:17:29 +0530

    Full project

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -7,6 +7,7 @@ import AnalysisPage from './views/AnalysisPage';
 import ResultsPage from './views/ResultsPage';
 import HistoryPage from './views/HistoryPage';
 import SettingsPage from './views/SettingsPage';
+import ROICalculatorPage from './views/ROICalculatorPage';
 import Layout from './components/Layout';
 import { User, SystemSettings, Notification } from './types';
 
@@ -143,6 +144,21 @@ const App: React.FC = () => {
                 settings={settings} 
                 onUpdateSettings={updateSettings} 
               />
+            </Layout>
+          ) : <Navigate to="/login" />} 
+        />
+        <Route 
+          path="/roi" 
+          element={user ? (
+            <Layout 
+              user={user} 
+              onLogout={handleLogout} 
+              searchTerm={searchTerm} 
+              setSearchTerm={setSearchTerm}
+              notifications={notifications}
+              onMarkRead={markNotificationsRead}
+            >
+              <ROICalculatorPage />
             </Layout>
           ) : <Navigate to="/login" />} 
         />
